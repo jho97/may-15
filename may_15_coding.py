2a4939f7ae2d20bfa6922769615480d697642f1e
Jonathan Ho <ho.jonathan97@gmail.com>
2022-05-16 10:32:44 -0500

    learning python

---
+++ may_15_coding.py
@@ -0,0 +1,33 @@
+# amount = 100
+# tax = .06
+
+# total = amount + amount*tax
+
+# print(total)
+
+
+# age calculator in decades
+# age = int(input("How old are you?\n"))
+
+# decades = int(age / 10)
+# year = int(age % 10) 
+
+# message = "You are " + str(decades) + " decades and " + str(year) + " year(s) old."
+
+# print(message)
+
+# Rock_Paper_Scissors
+# computer_choice = 'scissors'
+
+# user_choice = input("Rock, Paper, or Scissors?\n")
+
+# if computer_choice == user_choice:
+#     print("TIE")
+# elif user_choice ==  'rock' and computer_choice == 'scissors':
+#     print("You Win")
+# elif user_choice == 'paper' and computer_choice == 'rock':
+#     print("You Win")
+# elif user_choice == 'scissors' and computer_choice == 'paper':
+#     print("You Win")
+# else:
+#     print("You lose :(")
